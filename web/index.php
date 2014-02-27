85379424cd2062f1e751d349ef2101d926e52286
sylvain <sylvain@localhost.localdomain>
2014-02-27 23:50:59 +0100

    Mise en place de la recherche.

--- web/index.php
+++ web/index.php
@@ -25,17 +25,24 @@ $app->match('/', function (Request $request, Application $app){
     $query = new Query();
     $query->init($request->get('topic'));
     
-    
     //Caractéristiques
     $cpu = $request->get('CPU');
+    $ram = $request->get('RAM');
     
-    $queryBuilder = new QueryBuilder();
+    $queryBuilder = new QueryBuilder('$Reference');
+    $queryBuilder->select('$ID', '$Reference', '$Fabricant', '$Prix');
     $queryBuilder->where($queryBuilder->addOr('a08', 'r08a', 'r08b', $cpu));
+    $queryBuilder->addAnd($queryBuilder->addOr('a07', 'r07a', 'r07b', $ram));
+    //$queryBuilder->addAnd($Prix.'<500');
+    $queryBuilder->join('o01', '$Prix');
+    $queryBuilder->join('a01', '$Fabricant', 'r01b', 'r01a');
     
     $query->setQuery($queryBuilder->build());
     
     $csvManager = new Csv();
     $csvManager->import($query->execute());
+    
+    $result = $csvManager->export();
     
     $test = $request->get('query');
     
@@ -44,7 +51,8 @@ $app->match('/', function (Request $request, Application $app){
         'query' => $queryBuilder->build(),
         'topic' => $request->get('topic'),
         'fileContent' => $csvManager->getCsvString(),
-        'arrayContent' => $csvManager->export(),
+        'headers' => array_shift($result),
+        'entities' => $result,
     ));
 })
 ->method('GET|POST');
